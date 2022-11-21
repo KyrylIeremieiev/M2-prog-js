025fc151b30a193a0bb02d82f17ac09de74010fe
Kyryl <kieremieiev@outlook.com>
2022-11-21 10:27:36 +0100

    opdracht klaar

---
+++ les1/app.js
@@ -0,0 +1,18 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello $%^&");
+        let appNaam = "string"
+        let versienummer = 0.9
+        let versiedatum = "Date"
+        let autheur = "shithead"
+        let copyright = "no one"
+        let distributeur = "me"
+        let darkmode = true
+        console.log(appNaam, versienummer, versiedatum, autheur, copyright, distributeur, darkmode)
+}
+}
+
+let app = new App();
+app.runApplication();
